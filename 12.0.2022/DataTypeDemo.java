3c4c93d5b04714477f127f145ae815f447577975
littlesoul920 <115772463+littlesoul920@users.noreply.github.com>
2022-10-14 11:14:39 +0530

    Add files via upload

---
+++ 12.0.2022/DataTypeDemo.java
@@ -0,0 +1,16 @@
+/*program: to understand various data type
+@author: suman kumar
+@date: 14-10-2022
+*/
+
+
+class DataTypeDemo
+{
+   public static void main (String args[])
+   {
+	   int number1=77;
+	   int number2=67;
+	   int sum=number1+number2;
+   System.out.println("the sum of two number:"+sum);
+   }
+}
---
+++ 12.0.2022/aboutmyself.java
@@ -0,0 +1,12 @@
+class Aboutmyself
+{
+	public static void main(String args[])
+	{
+		System.out.println("NAME:"+args[0]+" "+args[1]);
+		System.out.println("Age:"+args[2]);
+		System.out.println("Course:"+args[3]);
+		System.out.println("Score:"+args[4]);
+	}
+}
+		
+		
